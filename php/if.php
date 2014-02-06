17405e5a9ec4cfb533a12307e7ee8a77a0a2ce8c
lesssliemarie <lesssliemarie@gmail.com>
2014-02-06 14:57:11 -0600

    Refactored if.php to if/elseif/else statements, changed variables

--- php/if.php
+++ php/if.php
@@ -1,47 +1,40 @@
 <?php
 
-$a = 5;
-$b = 10;
-$c = '10';
+$a = 8;
+$b = 11;
+$c = '11';
 
+//Shorten the next 2 statements into an if/else
 if ($a < $b) {
     // output the appropriate result
     echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
+} else {
     // output the appropriate result
-    echo "$b is greater than $a\n";
+    echo "$a is greater than $b\n";
 }
 
+
+//Shorten the next 2 statements into an if/else
 if ($b >= $c) {
     // output the appropriate result
     echo "$b is greater than or equal to $c\n";
-}
-
-if ($b <= $c) {
+} else {
     // output the appropriate result
     echo "$b is less than or equal to $c\n";
 }
 
-if ($b == $c) {
-    // output the appropriate result
-    echo "$b is equal to $c\n";
-}
 
+//combine the next 4 conditionals into
+//one if/elseif/else block that checks in order for:
+// identical, equal, not identical, not equal
 if ($b === $c) {
-    // output the appropriate result
     echo "$b is identical to $c\n";
-}
-
-// output the appropriate result
-if ($b != $c) {
-    echo "$b is not equal to $c\n";
-}
-
-// output the appropriate result
-if ($b !== $c) {
+} elseif ($b == $c) {
+    echo "$b is equal to $c\n";
+} elseif ($b !== $c) {
     echo "$b is not identical to $c\n";
-}
+} else {
+    echo "$b is not equal to $c\n";
+};
 
 ?>
