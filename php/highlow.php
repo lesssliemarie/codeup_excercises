2b6dc80f268db2ceb63ab48e5987d0b26bb6bf0e
lesssliemarie <lesssliemarie@gmail.com>
2014-02-07 11:53:06 -0600

    Completed highlow.php exercise
    Make a Higher or Lower number guessing game that prompts the user to
    guess a number between 1 and 100. Generate a random number as the
    answer and tell the user if they need to guess HIGHER or LOWER until
    they are correct. When correct, tell them GOOD GUESS!

---
+++ php/highlow.php
@@ -0,0 +1,29 @@
+<?php
+//game picks random number between 1 and 100
+$answer = rand(1, 100);
+
+// prompts user to guess the number
+fwrite(STDOUT, "Guess a number between 1 and 100\n");
+
+$playerGuess = fgets(STDIN);
+
+// loop through guessing process while $playerGuess is not equal to $answer
+while ($playerGuess != $answer) {
+	//GIVE HIGHER OR LOWER HINT
+	if ($playerGuess < $answer) {
+		echo "HIGHER! ";	
+	} else  {
+		echo "LOWER! ";
+	} 
+		
+	fwrite(STDOUT, "Guess again!\n");
+	$playerGuess = fgets(STDIN);
+
+} 
+
+// tell player they are right
+echo "GOOD GUESS!\n";	
+
+exit(0);
+
+?>
