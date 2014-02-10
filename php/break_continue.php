62eb260b13bd8ed341fc5019c4b915d6a7609185
lesssliemarie <lesssliemarie@gmail.com>
2014-02-10 14:41:54 -0600

    Completed Step 2 of break_continue.php
    Create a for loop that counts from 1 to 100, but stops at 10 using a
    break.

--- php/break_continue.php
+++ php/break_continue.php
@@ -2,12 +2,24 @@
 
 // create a for loop that shows all even numbers between 1 and 100
 // using continue
+// for ($i = 0; $i <= 100; $i++) {
+
+// 	if ($i % 2 != 0) {
+// 		continue;
+// 	}
+// 	echo $i . "\n";
+// }
+
+// create a for loop that counts from 1 to 100
+// stop it at 10 using a break
+
 for ($i = 0; $i <= 100; $i++) {
 
-	if ($i % 2 != 0) {
-		continue;
-	}
 	echo $i . "\n";
+
+	if ($i >= 10) {
+		break;
+	}
 }
 
 ?>
