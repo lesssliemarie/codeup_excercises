f9dca4893fa8c139048fc37116047c5f2a0ac328
lesssliemarie <lesssliemarie@gmail.com>
2014-02-10 13:44:29 -0600

    Completed Step 3 foreach_2.php
    Added if statement to output ONLY the books published after 1950.

--- php/foreach_2.php
+++ php/foreach_2.php
@@ -30,12 +30,25 @@ $books = array(
 // construct a loop that iterates through each book
 // then each book's keys and values
 // output the book's title, then list the key value pairs
+// ONLY for books written after 1950
+
+// foreach ($books as $title => $book) {
+// 	echo strtoupper("$title\n");
+// 		foreach ($book as $spec => $specInfo) {
+// 			echo "  $spec: $specInfo\n";
+// 		}
+// 	}
 
 foreach ($books as $title => $book) {
-	echo strtoupper("$title\n");
+
+	if ($book['published'] > 1950) {
+			echo strtoupper("$title\n");
+
 		foreach ($book as $spec => $specInfo) {
 			echo "  $spec: $specInfo\n";
 		}
 	}
+}
+
 
 ?>
