2d00e46af8e81bed91b917a0bd6da9812df5b0a1
lesssliemarie <lesssliemarie@gmail.com>
2014-02-10 12:10:36 -0600

    Completed Step 4 of foreach.php exercise
    Created a loop that echoes out every value in the array, including
    those in nested arrays.

--- php/foreach.php
+++ php/foreach.php
@@ -4,12 +4,22 @@
 $things = array('Sgt. Pepper', "11", null, array(1, 2, 3), 3.14, "12 + 7", false, (string) 11);
 
 // create a loop that iterates through the array
-// output ONLY the values that are scalar
+// echo out every value, including those nested in arrays
 foreach ($things as $thing) {
 	
-	if (is_scalar($thing)) {
-		echo "$thing\n";
+	if (is_array($thing)) {
+		echo "Array containing ";
+		foreach ($thing as $items) {
+			echo "$items ";
+		}	
 	}
+
+	if (!is_array($thing)) {
+	echo "$thing";		
+	}
+
+	echo "\n";
+
 }
 
 ?>
