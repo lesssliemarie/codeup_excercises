d107b9cc6e2a4f203a4c74df68d6d6895364d692
lesssliemarie <lesssliemarie@gmail.com>
2014-02-10 13:23:57 -0600

    Completed Step 2 of foreach_2.php
    Created an array of books. Created a loop that iterates through each
    book, outputs the title, and then outputs the value key pairs.

---
+++ php/foreach_2.php
@@ -0,0 +1,41 @@
+<?php
+// create an array of $books
+
+$books = array(
+	'The Hobbit' => array(
+		'published' => 1937,
+		'author' => 'J.R.R. Tolkien',
+		'pages' => 310
+		),
+
+	'Game of Thrones' => array(
+		'published' => 1996,
+		'author' => 'George R.R. Martin',
+		'pages' => 835
+		),
+
+	'The Catcher in the Rye' => array(
+		'published' => 1951,
+		'author' => 'J.D. Salinger',
+		'pages' => 220
+ 		),
+
+	'A Tale of Two Cities' => array(
+		'published' => 1859,
+		'author' => 'Charles Dickens',
+		'pages' => 544
+ 		),
+	);
+
+// construct a loop that iterates through each book
+// then each book's keys and values
+// output the book's title, then list the key value pairs
+
+foreach ($books as $title => $book) {
+	echo strtoupper("$title\n");
+		foreach ($book as $spec => $specInfo) {
+			echo "  $spec: $specInfo\n";
+		}
+	}
+
+?>
