30220cea88e04be384e7bf717d0819916aa03b27
lesssliemarie <lesssliemarie@gmail.com>
2014-02-10 11:26:34 -0600

    Completed Step 3 of foreach.php exercise
    Constructed a loop that iterates through the array and outputs ONLY the
    values that are scalar.

--- php/foreach.php
+++ php/foreach.php
@@ -4,32 +4,12 @@
 $things = array('Sgt. Pepper', "11", null, array(1, 2, 3), 3.14, "12 + 7", false, (string) 11);
 
 // create a loop that iterates through the array
-// output the type of each value
+// output ONLY the values that are scalar
 foreach ($things as $thing) {
-	if (is_float($thing)) {
-		echo "{$thing} is a float.\n";
+	
+	if (is_scalar($thing)) {
+		echo "$thing\n";
 	}
-
-	if (is_int($thing)) {
-		echo "{$thing} is a number\n";
-	}
-
-	if (is_bool($thing)) {
-		echo "\$thing is a boolean\n";
-	}
-
-	if (is_array($thing)) {
-		echo "\$thing is an array\n";
-	}
-
-	if (is_null($thing)) {
-		echo "\$thing is null\n";
-	}
-
-	if (is_string($thing)) {
-		echo "{$thing} is a string\n";
-	}
-
 }
 
 ?>
