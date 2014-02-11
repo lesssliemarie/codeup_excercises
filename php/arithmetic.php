a6327762adfc134f8ceb854913db4fe717edae51
lesssliemarie <lesssliemarie@gmail.com>
2014-02-11 13:14:04 -0600

    Added validation to divide by 0 and edited echo statements
    Nested another if/else inside divide to make sure user is not dividing
    by 0– outputs error message. Edited echo statements in each function to
    match their appropriate math operation.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -12,7 +12,7 @@ function subtract($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		echo $a - $b . "\n";	
 	} else {
-		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+		echo "ERROR: You didn't pass two numbers to subtract, silly goose!\n";
 	}	
 }
 
@@ -20,15 +20,19 @@ function multiply($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		echo $a * $b . "\n";	
 	} else {
-		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+		echo "ERROR: You didn't pass two numbers to multiply, silly goose!\n";
 	}	
 }
 
 function divide($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
-		echo $a / $b . "\n";	
+		if ($b != 0) {
+		 	echo $a / $b . "\n";
+		} else {
+			echo "You can't divide by 0!\n";
+		}
 	} else {
-		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+		echo "ERROR: You didn't pass two numbers to divide, silly goose!\n";
 	}	
 }
 
@@ -36,14 +40,14 @@ function modulus($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		echo $a % $b . "\n";	
 	} else {
-		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+		echo "ERROR: You didn't pass two numbers, silly goose!\n";
 	}	
 }
 
 add('cat', 2);
 subtract(10, 1);
 multiply(2, 6);
-divide(50, 'dog');
+divide(50, 0);
 modulus(10, 3);
 
 ?>
