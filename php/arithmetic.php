5d0bae67e1b130d2e70070e4aee7bad8d500ac09
lesssliemarie <lesssliemarie@gmail.com>
2014-02-11 13:07:00 -0600

    Added validation to arguments
    Added an if/else statement inside each function to check if users are
    passing numbers. If not, an error message is the output.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,29 +1,49 @@
 <?php
 
 function add($a, $b) {
-	echo $a + $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a + $b . "\n";	
+	} else {
+		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+	}	
 }
 
 function subtract($a, $b) {
-	echo $a - $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a - $b . "\n";	
+	} else {
+		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+	}	
 }
 
 function multiply($a, $b) {
-	echo $a * $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a * $b . "\n";	
+	} else {
+		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+	}	
 }
 
 function divide($a, $b) {
-	echo $a / $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a / $b . "\n";	
+	} else {
+		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+	}	
 }
 
 function modulus($a, $b) {
-	echo $a % $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a % $b . "\n";	
+	} else {
+		echo "ERROR: You didn't pass two numbers to add, silly goose!\n";
+	}	
 }
 
-add(1, 2);
+add('cat', 2);
 subtract(10, 1);
 multiply(2, 6);
-divide(50, 2);
+divide(50, 'dog');
 modulus(10, 3);
 
 ?>
