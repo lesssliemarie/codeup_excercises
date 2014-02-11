13402557545a1d3a40a8199321b35d4b6e8f3c12
lesssliemarie <lesssliemarie@gmail.com>
2014-02-11 14:24:00 -0600

    Refactored the error messages into a function
    Wrote validate function, added it to other functions. If any numbers
    aren’t passed, user is kicked out to try again.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,53 +1,48 @@
 <?php
 
+function validate($a, $b) {
+	if (!is_numeric($a) || !is_numeric($b)) {
+		echo "ERROR: You need two NUMBERS to do math, smartie.\nDo these both look like numbers?\n";
+		var_dump($a);
+		var_dump($b);
+		echo "I'm kicking you out. Try again!\n";
+		exit(0);
+	} 
+}
+
 function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a + $b . "\n";	
-	} else {
-		echo "ERROR: You can't add $a and $b together, silly goose!\n";
-	}	
+	validate($a, $b);
+ 	echo $a + $b . "\n";		
 }
 
 function subtract($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a - $b . "\n";	
-	} else {
-		echo "ERROR: You can't subtract $b from $a, silly goose!\n";
-	}	
+	validate($a, $b);
+	echo $a - $b . "\n";	
 }
 
 function multiply($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a * $b . "\n";	
-	} else {
-		echo "ERROR: You can't multiply $a and $b, silly goose!\n";
-	}	
+	validate($a, $b);
+	echo $a * $b . "\n";		
 }
 
 function divide($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		if ($b != 0) {
-		 	echo $a / $b . "\n";
-		} else {
-			echo "You can't divide by 0! DUH\n";
-		}
+	validate($a, $b);
+	if ($b != 0) {
+		 echo $a / $b . "\n";
 	} else {
-		echo "ERROR: You can't divide $b by $a, silly goose!\n";
-	}	
+		echo "You can't divide by 0! DUH\n";
+	}		
 }
 
 function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a % $b . "\n";	
-	} else {
-		echo "ERROR: You can't get the remainder of $a divided by $b, silly goose!\n";
-	}	
+	validate($a, $b);
+	echo $a % $b . "\n";		
 }
 
 add('cat', 2);
 subtract(10, 1);
 multiply(2, 'cheese');
 divide(50, 0);
-modulus(10, 3);
+modulus(3, 10);
 
 ?>
