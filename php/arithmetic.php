9a244c0ec864182c5fe97d59960c0560ed0d885a
lesssliemarie <lesssliemarie@gmail.com>
2014-02-11 11:04:20 -0600

    Completed Step 2 of arithmetic.php
    Added code after functions to call functions and pass real numbers.
    Added new line to each function’s echo statement.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,19 +1,24 @@
 <?php
 
 function add($a, $b) {
-	echo $a + $b;
+	echo $a + $b . "\n";
 }
 
 function subtract($a, $b) {
-	echo $a - $b;
+	echo $a - $b . "\n";
 }
 
 function multiply($a, $b) {
-	echo $a * $b;
+	echo $a * $b . "\n";
 }
 
 function divide($a, $b) {
-	echo $a / $b;
+	echo $a / $b . "\n";
 }
+
+add(1, 2);
+subtract(10, 1);
+multiply(2, 6);
+divide(50, 2);
 
 ?>
