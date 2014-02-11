8d2a0b5be41076cbbac4c07e634de3ff16dd08a0
lesssliemarie <lesssliemarie@gmail.com>
2014-02-11 16:58:29 -0600

    Created and tested internal-functions.php
    Creates function to check if a variable is set or empty and displays
    the variable name. Runs tests in comments. Serializes and unserializes
    an array.

---
+++ php/internal-functions.php
@@ -0,0 +1,48 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+// Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+
+function setOrEmpty($name, $var) {
+	if (isset($var)) {
+		echo "The variable $name is set\n";
+	} elseif (empty($var)) {
+		echo "The variable $name is empty\n";
+	}
+}
+
+setOrEmpty('$nothing', $nothing);
+setOrEmpty('$something',$something);
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+if (isset($nothing)) {
+	echo "\$nothing is SET\n";
+} 
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+if (empty($nothing)) {
+	echo "\$nothing is EMPTY\n";
+}
+
+// TEST: If var $something is set, display '$something is SET'
+
+if (isset($something)) {
+	echo "\$something is SET\n";
+}
+
+// Serialize the array $array, and output the results
+
+$serialize = serialize($array);
+echo "This is a serialized array:\n" . $serialize . "\n";
+
+// Unserialize the array $array, and output the results
+
+$unserialize = unserialize($serialize);
+var_dump($unserialize) . "\n";
+
+?>
