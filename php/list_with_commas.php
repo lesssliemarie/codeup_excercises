cc05a04ca289b75708609f8ca0c969f35a3874eb
lesssliemarie <lesssliemarie@gmail.com>
2014-02-13 14:26:29 -0600

    Set the sorting default to false
    Set sorting default to false so that if no second argument is passed,
    no sorting occurs.

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -1,7 +1,7 @@
 <?php
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($input, $sort) {
+function humanized_list($input, $sort = FALSE) {
 
 	$input = explode(', ', $input);
 	
@@ -18,7 +18,7 @@ function humanized_list($input, $sort) {
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 // Humanize that list
-$famous_fake_physicists = humanized_list($physicists_string, FALSE);
+$famous_fake_physicists = humanized_list($physicists_string);
 
 // Output sentence
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
