debd6dd16c5aa9b59e3759e6ec8a64768da1133e
lesssliemarie <lesssliemarie@gmail.com>
2014-02-18 16:54:37 -0600

    Initial commit

---
+++ codeup_challenges/palindrome.php
@@ -0,0 +1,17 @@
+<?php
+
+// Create a function so that it will return a bool value true
+// If the word is a palindrome
+
+function is_palindrome($word) {
+
+}
+
+// Test several known palindromes
+is_palindrome('Amore, Roma');
+is_palindrome('A man, a plan, a canal: Panama');
+is_palindrome("No 'x' in 'Nixon'");
+
+// Test regular words
+
+?>
