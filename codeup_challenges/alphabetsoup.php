c87037672e1c0ae0d98e07f15def7eb10ea10b4a
lesssliemarie <lesssliemarie@gmail.com>
2014-02-19 16:54:46 -0600

    Initial commit

---
+++ codeup_challenges/alphabetsoup.php
@@ -0,0 +1,13 @@
+<?php
+// Create a funciton that accpets a string
+// and will return the string in alphabetical order
+// EX: "hello world" becomes "ehllo dlorw"
+// Separate and alphabetize each word individually
+
+function alphabet_soup($str) {
+
+}
+
+alphabet_soup("hello world");
+
+?>
