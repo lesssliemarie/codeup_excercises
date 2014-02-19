f60edef3f519cbdae1994ce57b6d16679e5dced9
lesssliemarie <lesssliemarie@gmail.com>
2014-02-18 18:13:54 -0600

    Completed palindrome challenge and tested

--- codeup_challenges/palindrome.php
+++ codeup_challenges/palindrome.php
@@ -4,14 +4,32 @@
 // If the word is a palindrome
 
 function is_palindrome($word) {
+	$word = strtolower(str_replace(' ', '', $word));
+	$reverse = strrev($word);
+	if ($word == $reverse) {
+		return TRUE;
+	} else {
+		return FALSE;
+	}
+}
 
+function runTest($word) {
+	if (is_palindrome($word)) {
+		echo $word . ' is a palindrome! ' . strrev($word) . PHP_EOL;
+	} else {
+		echo $word . ' is NOT a palindrome :(' . PHP_EOL;
+	}
 }
 
 // Test several known palindromes
-is_palindrome('Amore, Roma');
-is_palindrome('A man, a plan, a canal: Panama');
-is_palindrome("No 'x' in 'Nixon'");
+runTest('Amore Roma');
+runTest('A man a plan a canal Panama');
+runTest("No x in Nixon");
 
 // Test regular words
+
+runTest('Jimi');
+runTest('Leslie Marie');
+runTest('Stressed Desserts');
 
 ?>
