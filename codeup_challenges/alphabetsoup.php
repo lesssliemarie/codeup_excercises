6709921c8872d4548bb8eb798b36e771f0d9e980
lesssliemarie <lesssliemarie@gmail.com>
2014-02-21 14:57:35 -0600

    Completed alphabet_soup function and tested

--- codeup_challenges/alphabetsoup.php
+++ codeup_challenges/alphabetsoup.php
@@ -5,9 +5,26 @@
 // Separate and alphabetize each word individually
 
 function alphabet_soup($str) {
-
+	// split string by word into an array
+	$strArray = str_word_count($str, 1);
+	$soupSentence = '';
+	// iterate over array 
+	foreach($strArray as $word) {
+		// split words into separate arrays and sort
+		$wordArray = str_split($word);
+		sort($wordArray);
+		// implode them back to strings and add to empty soupSentence
+		$word = implode('', $wordArray);
+		$soupSentence .= $word . ' ';
+		
+	}
+	return $soupSentence . PHP_EOL;
 }
 
-alphabet_soup("hello world");
+echo alphabet_soup("hello world");
+echo alphabet_soup("leslie marie");
+echo alphabet_soup("jimi jims");
+echo alphabet_soup("hello codeup");
+echo alphabet_soup("tgif tgif tgif");
 
 ?>
