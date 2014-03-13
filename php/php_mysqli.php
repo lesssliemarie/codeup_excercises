6e0cdebd4c0bac67902013724bbd3c9fb6e9d4c0
lesssliemarie <lesssliemarie@gmail.com>
2014-03-13 14:18:54 -0500

    Initial commit

---
+++ php/php_mysqli.php
@@ -0,0 +1,13 @@
+<?php
+// Connect to database
+$mysqli = @new mysqli('127.0.0.1', 'queenbee', 'b8z92u9z', 'codeup_test_db');
+
+// Check for errors
+if ($mysqli->connect_errno) {
+	echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error . PHP_EOL;
+} else {
+	echo $mysqli->host_info . PHP_EOL;
+}
+
+
+?>
